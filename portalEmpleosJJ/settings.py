95285bdad3c88c51f170e37961c9d5e24b00214b
Jennifer Defaz <jenniferdefaz991@gmail.com>
2026-06-22 01:34:15 -0500

    Plantilla JobBoard completamente funcional: estilos, imágenes y testimonios corregidos

--- portalEmpleosJJ/settings.py
+++ portalEmpleosJJ/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'empleos',
+    'empleo',
 ]
 
 MIDDLEWARE = [
@@ -116,3 +117,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS=(os.path.join(BASE_DIR,'portalEmpleosJJ/static'),) # Este va a ser una constante 
