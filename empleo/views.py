d6be2f133635a4ae0df689ac617a07f3a6f5de3e
Jennifer Defaz <jenniferdefaz991@gmail.com>
2026-06-22 00:15:07 -0500

    Creacion del urls.py dentro de la app empleo y agregando la funcion inicio en views.py

--- empleo/views.py
+++ empleo/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def inicio(request):
+    #Presentando en pantalla el contenido de inicio s
+    return render(request, 'inicio.html')
